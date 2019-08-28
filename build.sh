9a97916db8d82266884acb60f912c03f093e2a48
root <root@wlp7.sh.intel.com>
2019-08-28 13:35:43 -0700

    add mklml downloading to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#wget https://github.com/intel/mkl-dnn/releases/download/v0.20/mklml_lnx_2019.0.5.20190502.tgz
-#tar -xzvf ./mklml_lnx_2019.0.5.20190502.tgz
+wget https://github.com/intel/mkl-dnn/releases/download/v0.20/mklml_lnx_2019.0.5.20190502.tgz
+tar -xzvf ./mklml_lnx_2019.0.5.20190502.tgz
 ln -s ./mklml_lnx_2019.0.5.20190502 ./mklml
 
 
